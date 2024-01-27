8f4b73588fb02bd857c557aa3aaba42365824afe
viacheslavk55 <525vkostenko@frhsd.com>
2024-01-27 15:51:52 -0500

    wrote js code and added images

---
+++ script.js
@@ -0,0 +1,54 @@
+// Changing into 1841
+function changeStuff1() {
+    var element = document.getElementById("changeTitle")
+    element.textContent = "April 4th, 1841"
+    var element = document.getElementById("changeText");
+    element.textContent = "William Henry Harrison, the ninth President of the United States, died on April 4, 1841, just 32 days into his presidency, making it the shortest presidential term in history. His death was attributed to pneumonia, believed to have been contracted during his lengthy inaugural address delivered in inclement weather."
+    var imageElement = document.getElementById("changeImage");
+    imageElement.src = "images/william.png";
+}
+// Changing into 1850
+function changeStuff2() {
+    var element = document.getElementById("changeTitle")
+    element.textContent = "April 4th, 1850"
+    var element = document.getElementById("changeText");
+    element.textContent = "Los Angeles became an American city on April 4, 1850, when California was admitted as the 31st state of the United States. The city's incorporation marked the beginning of its growth into one of the largest and most influential urban centers in the world."
+    var imageElement = document.getElementById("changeImage");
+    imageElement.src = "images/los angeles.png";
+}
+// Changing into 1949
+function changeStuff3() {
+    var element = document.getElementById("changeTitle")
+    element.textContent = "April 4th, 1949"
+    var element = document.getElementById("changeText");
+    element.textContent = "NATO, the North Atlantic Treaty Organization, was formed on April 4, 1949, with the signing of the North Atlantic Treaty. It is a military alliance of 30 countries, established to ensure the collective defense of its members against aggression."
+    var imageElement = document.getElementById("changeImage");
+    imageElement.src = "images/nato.png";
+}
+// Changing into 1968
+function changeStuff4() {
+    var element = document.getElementById("changeTitle")
+    element.textContent = "April 4th, 1968"
+    var element = document.getElementById("changeText");
+    element.textContent = "Martin Luther King Jr. was tragically assassinated on April 4, 1968, in Memphis, Tennessee, marking a devastating loss for the civil rights movement. His death continues to be a poignant reminder of the ongoing struggle for racial equality and justice in the United States."
+    var imageElement = document.getElementById("changeImage");
+    imageElement.src = "images/martin.png";
+}
+// Changing into 1975
+function changeStuff5() {
+    var element = document.getElementById("changeTitle")
+    element.textContent = "April 4th, 1975"
+    var element = document.getElementById("changeText");
+    element.textContent = "Microsoft was founded on April 4, 1975, by Bill Gates and Paul Allen in Albuquerque, New Mexico. The company became a global technology giant, known for its software products such as the Windows operating system and Microsoft Office suite."
+    var imageElement = document.getElementById("changeImage");
+    imageElement.src = "images/bill.png";
+}
+// Changing into 2007
+function changeStuff6() {
+    var element = document.getElementById("changeTitle")
+    element.textContent = "April 4th, 2007"
+    var element = document.getElementById("changeText");
+    element.textContent = "Viacheslav Kostenko was born in Kyiv, Ukraine. He has 2 brothers and is the middle child. 16 years later he lives in the United States of America, and attends Marlboro High School. He also is the creator of this website."
+    var imageElement = document.getElementById("changeImage");
+    imageElement.src = "images/me.png";
+}
